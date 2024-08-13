436370566e0f1a4055753ce625f4d7905691b974
kevit-gungun-udhani <gungun.udhani@kevit.io>
2024-08-13 19:04:26 +0530

    Form pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import FormPage from './pages/Form'
 function App() {
 
   return (
     <>
       <Outlet/>
+      <FormPage/>
     </>
   )
 }
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,7 +1,20 @@
+
 export default function FormPage(){
+   
+    function handleFormAction(event, formData){
+        event.preventDefault();
+        console.log(formData)
+    }
+
+
     return(
-        <div>
-            Form
-        </div>
+        <form onSubmit={(event) => {handleFormAction(event)}}>
+            <label>
+                Full Name
+                <input type="text" name="userName" required/>
+            </label>
+            <button type="submit">Submit</button>
+        </form> 
     )
 }
+
