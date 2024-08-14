f5e4f7873f43d4243423185880c09369b4635778
kevit-gungun-udhani <gungun.udhani@kevit.io>
2024-08-14 12:23:44 +0530

    feat/Form structure and css

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,20 +1,45 @@
+import classes from "./Form.module.css";
+export default function FormPage() {
+  
+  return (
+    <div className={classes.wrapper}>
+      <form className={classes.form}>
+        <label htmlFor="fullName">Full Name:</label>
+        <input type="text" name="fullName" id="fullName" className={classes.input} />
 
-export default function FormPage(){
-   
-    function handleFormAction(event, formData){
-        event.preventDefault();
-        console.log(formData)
-    }
+        <fieldset>
+          <legend>Select Gender:</legend>
 
+            <input type="radio" name="Male" id="Male" className={classes.radioInput}/>
+            <label htmlFor="Male">Male:</label>
+         
+            <input type="radio" name="Female" id="Female" className={classes.radioInput}/>
+            <label htmlFor="Female">Female:</label>
+         
+        </fieldset>
 
-    return(
-        <form onSubmit={(event) => {handleFormAction(event)}}>
-            <label>
-                Full Name
-                <input type="text" name="userName" required/>
-            </label>
-            <button type="submit">Submit</button>
-        </form> 
-    )
+        <label htmlFor="PhoneNo">Phone No.</label>
+        <input
+          type="tel"
+          name="PhoneNo"
+          id="PhoneNo"
+          pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+          className={classes.input}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email" className={classes.input} />
+
+        <label htmlFor="language">Choose a language:</label>
+        <select id="language" name="language" className={classes.input}>
+            <option value="Java">Java</option>
+            <option value="Python">Python</option>
+            <option value="C">C</option>
+            <option value="C++">C++</option>
+        </select>
+
+        <button type="submit" className={classes.submit}>Submit</button>
+      </form>
+    </div>
+  );
 }
-
